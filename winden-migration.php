bfedb8f5d9dfc24529a1c833d0978dde6ac9dd39
Marko Krstic <mailme@markokrstic.com>
2024-02-20 14:04:10 +0100

    Update winden-migration.php

--- winden-migration.php
+++ winden-migration.php
@@ -6,8 +6,8 @@
  * Version:           0.0.2
  * Requires at least: 5.2
  * Requires PHP:      7.4
- * Author:            Marko Krstic
- * Author URI:        https://author.example.com/
+ * Author:            DPlugins
+ * Author URI:        https://dplugins.com/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Update URI:        https://example.com/tailwind-cdn-for-gutenberg/
